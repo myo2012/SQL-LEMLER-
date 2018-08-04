c97ebd10f2b1d9252263413898e0cc606fe7273e
myo2012 <42101273+myo2012@users.noreply.github.com>
2018-08-05 01:22:56 +0300

    Add files via upload

---
+++ sqlişlemleri/sqlişlemleri/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace sqlişlemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dg = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(182, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(195, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = " soyad";
+            // 
+            // dg
+            // 
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Location = new System.Drawing.Point(12, 94);
+            this.dg.Name = "dg";
+            this.dg.Size = new System.Drawing.Size(416, 234);
+            this.dg.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(321, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = " dgde göster";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(191, 334);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 7;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(272, 334);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 8;
+            this.btnNext.Text = " Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(353, 334);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 9;
+            this.btnLast.Text = " Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(101, 334);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 10;
+            this.btnFirst.Text = " First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 11;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(154, 65);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(69, 65);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(27, 378);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(133, 376);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "ID SİL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(224, 379);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 16;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(330, 379);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "ada göre liste";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(549, 241);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(126, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = " textlerdegöster";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(549, 133);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(134, 20);
+            this.textBox5.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(549, 170);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(134, 20);
+            this.textBox6.TabIndex = 20;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(549, 200);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(134, 20);
+            this.textBox7.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 504);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dg;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ sqlişlemleri/sqlişlemleri/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace sqlişlemleri
+{
+    public partial class Form1 : Form
+    {
+
+        DataSet ds = new DataSet(); 
+        SqlConnection conn = new SqlConnection("Data Source=CODER\\SQLEXPRESS;Initial Catalog=YOUTUBE;Integrated Security=TRUE");
+        SqlDataAdapter da = new SqlDataAdapter();
+        BindingSource tablo = new BindingSource(); //3
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*conn.Open();
+            MessageBox.Show(conn.State.ToString());
+            conn.Close();*/
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            da.InsertCommand = new SqlCommand("INSERT INTO tablo VALUES(@ad,@soyad)", conn);
+            da.InsertCommand.Parameters.Add("@ad", SqlDbType.VarChar).Value = textBox1.Text;
+            da.InsertCommand.Parameters.Add("@soyad",SqlDbType.VarChar).Value=textBox2.Text;
+
+            conn.Open();
+
+            da.InsertCommand.ExecuteNonQuery();
+
+            conn.Close();
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            da.SelectCommand = new SqlCommand("SELECT * FROM tablo", conn);
+            ds.Clear();
+
+            da.Fill(ds);
+
+            dg.DataSource = ds.Tables[0];
+
+            tablo.DataSource = ds.Tables[0]; //3
+
+            textBox1.DataBindings.Add(new Binding("Text", tablo, "ad"));
+            textBox2.DataBindings.Add(new Binding("Text", tablo, "soyad"));
+            goster();
+
+
+
+
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            tablo.MoveNext();
+            dgUpdate();
+            goster();
+
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            tablo.MovePrevious();
+            dgUpdate();
+            goster();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            tablo.MoveFirst();
+            dgUpdate();
+            goster();
+
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            tablo.MoveLast();
+            dgUpdate();
+            goster();
+        }
+
+        private void dgUpdate()
+        {
+
+            dg.ClearSelection();
+            dg.Rows[tablo.Position].Selected = true;
+            goster();
+
+        }
+
+        private void goster()
+        {
+            label3.Text = "Kayıtlar  " + (tablo.Position +1 )+ " - " + (tablo.Count);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int x;
+
+
+            da.UpdateCommand = new SqlCommand("UPDATE tablo SET ad=@ad,soyad=@soyad WHERE ID=@ID", conn);
+            da.UpdateCommand.Parameters.Add("@ad", SqlDbType.VarChar).Value = textBox1.Text;
+            da.UpdateCommand.Parameters.Add("@soyad", SqlDbType.VarChar).Value = textBox2.Text;
+            da.UpdateCommand.Parameters.Add("@ID", SqlDbType.Int).Value = ds.Tables[0].Rows[tablo.Position][0];
+
+            conn.Open();
+            x=da.UpdateCommand.ExecuteNonQuery();
+            ds.Clear();
+            da.Fill(ds);
+
+            conn.Close();
+
+            if (x >= 1)
+                MessageBox.Show("Düzenleme  işlemi tamamlanmıştır");
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            DialogResult dr;
+
+            dr = MessageBox.Show("Silmek istediğinizden Eminmisiniz ?\n Veri tabanından kayıt siliyorsunuz", "Karar Anı", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes)
+            {
+
+                da.DeleteCommand = new SqlCommand("DELETE FROM tablo WHERE ID=@ID", conn);
+                da.DeleteCommand.Parameters.Add("@ID", SqlDbType.Int).Value = ds.Tables[0].Rows[tablo.Position][0];
+
+                conn.Open();
+
+                da.DeleteCommand.ExecuteNonQuery();
+                ds.Clear();
+
+                da.Fill(ds);
+
+                conn.Close();
+            }
+            else
+            {
+
+                MessageBox.Show("silme işlemi durdururuldu");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            da.DeleteCommand = new SqlCommand("DELETE FROM tablo WHERE ID=@ID", conn);
+            da.DeleteCommand.Parameters.Add("@ID", SqlDbType.Int).Value = textBox3.Text;
+            conn.Open();
+            
+
+            da.DeleteCommand.ExecuteNonQuery();
+           
+            ds.Clear();
+
+            da.Fill(ds);
+
+            conn.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            da.SelectCommand = new SqlCommand("SELECT * FROM tablo WHERE ad=@ad", conn);
+            da.SelectCommand.Parameters.Add("@ad", SqlDbType.VarChar).Value = textBox4.Text;
+            conn.Open();
+            ds.Clear();
+            da.Fill(ds,"tablo");
+            dg.DataSource = ds.Tables["tablo"];
+            
+            conn.Close();
+           
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+            SqlCommand cmd = new SqlCommand("SELECT ad,soyad FROM tablo WHERE ID=@ID", conn);
+            cmd.Parameters.Add("@ID",SqlDbType.Int).Value=textBox5.Text;
+            conn.Open();
+            try
+            {
+            SqlDataReader dr = cmd.ExecuteReader();
+           
+                if (dr.Read())
+                {
+                    textBox6.Text = dr["ad"].ToString();
+                    textBox7.Text = dr["soyad"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Aradığınız Kayıt bulunamadı");
+                }
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show("Bir hata meydana geldi bu yüzden işlem gerçekleşemiyor"+hata);
+            }
+         
+     
+            conn.Close();
+  
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
